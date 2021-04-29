37286d692d2f52601597c6aa7dd7af587ad46702
AndradeL <l182371@dac.unicamp.br>
2021-04-28 22:54:25 -0300

    define estruturas de dados para o protocolo de comunicação

---
+++ comm.h
@@ -0,0 +1,35 @@
+#include <inttypes.h>
+
+#define MAX_CHARS 100
+#define MAX_EXP 20
+
+typedef enum _operation {
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE
+} Operation;
+
+typedef struct _userProfile {
+    char email[MAX_CHARS];
+    char nome[MAX_CHARS];
+    char sobrenome[MAX_CHARS];
+    char residencia[MAX_CHARS];
+    char formacao[MAX_CHARS];
+    int32_t ano_formatura;
+    char habilidades[MAX_CHARS * 2];
+    char experiencia[MAX_EXP][MAX_CHARS];
+} UserProfile;
+
+typedef struct _protocolData {
+    Operation op;
+    uint32_t profiles_num;
+    // UserProfile* profiles_buf; // subsequent data is a user profile buffer
+} ProtocolData;
+
+// 4 / 4 / 4 / ***********************************************
+//    void* buf;
+//    ProtocolData* data = (ProtocolData*)buf;
+//    data->op;           // 4 bytes
+//    data->profiles_num; // 4 bytes
+//    UserProfile* prof_buf = (UserProfile*) (buf + 8);
